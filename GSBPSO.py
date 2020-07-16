2f511ddede36f2ab87cda840748de352bdd3346f
handsomelys <m13502254993@163.com>
2020-07-16 23:06:31 +0800

    almost done

--- GSBPSO.py
+++ GSBPSO.py
@@ -1,58 +1,101 @@
 import numpy as np
 import random
+from TFIDF import TextVectorizer
+import math
 from math import e
 #计算余弦相似度
 # 适应值函数未编写完成
 # 更新粒子的位置和速度未完成
-def cosine_similarity(vec1,vec2):
-    dist = float(np.dot(vec1,vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2)))
-    return dist
-
-def getRandom(size):
-    m_max,m_min = 0
-    while true:
-        m_1 = random.uniform(1,size)
-        m_2 = random.uniform(1,size)
-        if m_1 != m_2:
-            break
-    if m_1 > m_2:
-        m_max = m_1
-        m_min = m_2
+def cosine_similarity(x,y):
+    '''
+    vc = 0
+    for i in range(1,len(x)-2):
+        xc1 = x[i] - x[i-1]
+        xc2 = x[i+1] - x[i]
+        yc1 = y[i] - y[i-1]
+        yc2 = y[i+1] - y[i]
+        vc = (xc1*xc2+yc1*yc2)/(math.sqrt(xc1**2+yc1**2)*math.sqrt(xc2**2+yc2**2))
+        #print(float(vc[0]))
+    return vc
+    '''
+    num = float(np.matmul(x, y))
+    s = np.linalg.norm(x) * np.linalg.norm(y)
+    if s == 0:
+       result = 0.0
     else:
-        m_max = m_2
-        m_min = m_1
-    return m_max,m_min
-    
-def fit_fun(particles):
-    m = []   #随机取 ？m<=size
+          result = num/s
+        
+    return float(result)
+
+
+def encode_of_textvector(textvector:list,chrom_a: list,chrom_b:list):
+    '''
+    textvector:关键词的文本向量，对应论文中的TF-IDF计算出的原始文本向量；
+    chrom_a:代表染色体C1；
+    chrom_b:代表染色体C2
+    '''
+    #print("="*60)
+    #print(chrom_a)
+    #print(chrom_b)
+    union = [i | j for i, j in zip(chrom_a, chrom_b)] # 求得两条染色体的并集
+    #print(union)
+    dC1 = []
+    dC2 = []
+    for i in range(len(union)):
+        if union[i]==1:
+            if chrom_a[i]==1:
+                dC1.append(textvector[i])
+            else:
+                dC1.append(0)
+            if chrom_b[i] == 1:
+                dC2.append(textvector[i])
+            else:
+                dC2.append(0)
+    #print(dC1)
+    #print(dC2)
+    return dC1,dC2
+
+def fit_fun(particles,x):
+    fit_value_dict = {} 
+    m = []   
     size = particles.getSize()
-    m_max,m_min = getRandom(size)
-    for i in range(m_min,m_max):
-        m.append(int(i))
-    fit_value = 0
+    #m_max,m_min = getRandom(size)
+    for i in range(x):
+        m.append(int(random.uniform(0,size)))   #获得需要更新适应值的个体
+    
     for i in m:
-        for j in size:
-            dist = cosine_similarity(particles.getPatical_list()[i].getPos(),particles.getPatical_list()[j].getPos())
-            fit_value = fit_value + dist
-    return fit_value
+        fit_value = 0
+        for j in range(size):
+            #fit_value = cosine_similarity(particles.getPatical_list()[i].getPos(),particles.getPatical_list()[j].getPos())
+            dc1,dc2 = encode_of_textvector(feature_tfidf,particles.getPatical_list()[i].getPos(),particles.getPatical_list()[j].getPos())
+            value = cosine_similarity(dc1,dc2)
+            fit_value = fit_value + value
+        if fit_value > particles.getPatical_list()[i].getFitness_value():
+            particles.getPatical_list()[i].setBest_pos(particles.getPatical_list()[i].getPos())
+        particles.getPatical_list()[i].setFitness_value(fit_value)
+        fit_value_dict[i] = fit_value
+    fit_value_sort = sorted(fit_value_dict.items(), key=lambda x: x[1], reverse=True)
+
+    return fit_value_sort
 
 
 class Particle:
-    def __init__(self,x_max,max_vel,dim):
+    def __init__(self,max_vel,dim):
         super().__init__()
-        self._pos = [random.uniform(-x_max,x_max) for i in range(dim)]
+        self._pos = [np.random.randint(0,2) for i in range(dim)]
         self._vel = [random.uniform(-max_vel,max_vel) for i in range(dim)]
-        self._best_pos = [0.0 for i in range(dim)]
+        self._best_pos = [0 for i in range(dim)]
         #self._fitness_value = fit_fun(self._pos)
-    
+        self._fitness_value = float('-inf')
+
     def setPos(self,i,value):
         self._pos[i] = value
     
     def getPos(self):
         return self._pos
 
-    def setBest_pos(self,i,value):
-        self._best_pos[i] = value
+    def setBest_pos(self,value):
+        self._best_pos = value
 
     def getBest_pos(self):
         return self._best_pos
@@ -70,15 +113,16 @@ class Particle:
         return self._fitness_value
     
 class GSBPSO:
-    def __init__(self,dim,size,iter_num,x_max,max_vel,theta,gama,best_fitness_value=float('-Inf'),c1=2,c2=2,w=1):
+    def __init__(self,dim,size,iter_num,max_vel,theta,gama,best_fitness_value=float('-Inf'),c1=2,c2=2,w=1):
         super().__init__()
+        
         self.c1 = c1
         self.c2 = c2
         self.w = w
         self.dim = dim  #粒子的维度
         self.size = size    #粒子个数
         self.iter_num = iter_num    #迭代次数
-        self.x_max = x_max  #粒子范围
+        #self.x_max = x_max  #粒子范围
         self.max_vel = max_vel  #粒子最大速度
         self.best_fitness_value = best_fitness_value
         self.best_position = [0.0 for i in range(dim)]  #种群最优位置
@@ -87,7 +131,7 @@ class GSBPSO:
         self.gama = gama
 
         #初始化种群
-        self.patical_list = [Particle(self.x_max,self.max_vel,self.dim) for i in range(self.size)]
+        self.patical_list = [Particle(self.max_vel,self.dim) for i in range(self.size)]
 
     def getSize(self):
         return self.size
@@ -121,8 +165,12 @@ class GSBPSO:
 
     def update_vel(self,part):
         for i in range(self.dim):
-            vel_value = self.w * part.getVel()[i] + self.c1 * random.random() * (part.getBest_pos()[i] - part.getPos()[i])\
-                + self.c2 * random.random() * (self.getBest_position()[i] - part.getPos()[i])
+            #print(part)
+            #print(part.getVel())
+            #print(part.getBest_pos())
+            #print(self.getBest_position())
+            #print(part.getPos())
+            vel_value = self.w * part.getVel()[i] + self.c1 * random.random() * (part.getBest_pos()[i] - part.getPos()[i]) + self.c2 * random.random() * (self.getBest_position()[i] - part.getPos()[i])
             if vel_value > self.max_vel:
                 vel_value = self.max_vel
             elif vel_value < -self.max_vel:
@@ -131,27 +179,18 @@ class GSBPSO:
             
 
     def update_pos_global(self,part):   #更新位置 侧重全局搜索能力
-        for i in range(self,dim):
-            if abs(part.getVel()) >= self.theta:
-                if part.getVel() >= 0 and self.getBest_position()[i] == 0:
+        for i in range(self.dim):
+            if abs(part.getVel()[i]) >= self.theta:
+                if part.getVel()[i] >= 0 and self.getBest_position()[i] == 0:
                     part.setPos(i,0)
-                elif abs(part.getVel()) <= 0 and self.getBest_position()[i] == 1:
+                elif abs(part.getVel()[i]) <= 0 and self.getBest_position()[i] == 1:
                     part.setPos(i,1)
-            elif abs(part.getVel()) < self.theta:
-                if random.random() < s_function_global(part,i):
+            elif abs(part.getVel()[i]) < self.theta:
+                if random.random() < self.s_function_global(part,i):
                     part.setPos(i,1)
                 else:
                     part.setPos(i,0)
-        value = fit_fun(self)
-        if value < part.getFitness_value():
-            part.setFitness_value(value)
-            for i in range(self.dim):
-                part.setBest_pos(i,part.getPos()[i])
-        if value <self.getBest_fitness_value():
-            self.setBest_fitness_value(value)
-            for i in range(self.dim):
-                self.setBest_position(i,part.getPos()[i])
-    
+
     def update_pos_part(self,part):
         for i in range(self,dim):
             if part.getVel()[i] < 0:
@@ -172,12 +211,18 @@ class GSBPSO:
                 for part in self.patical_list:
                     self.update_vel(part)
                     self.update_pos_global(part)
-                self.fitness_val_list.append(self.getBest_fitness_value())
+                #self.fitness_val_list.append(self.getBest_fitness_value())
             else:
                 for part in self.patical_list:
                     self.update_vel(part)
                     self.update_pos_part(part)
-                self.fitness_val_list.append(self.getBest_fitness_value())
+                #self.fitness_val_list.append(self.getBest_fitness_value())
+            update_fitvalue = fit_fun(self,5)
+            if update_fitvalue[0][1] > self.best_fitness_value:
+                self.best_fitness_value = update_fitvalue[0][1]
+                self.best_position = self.getPatical_list()[update_fitvalue[0][0]]
+                self.fitness_val_list.append(self.best_fitness_value)
+
         return self.fitness_val_list, self.getBest_position()
     
 
@@ -189,3 +234,33 @@ class GSBPSO:
             return 1 - 2/(1+pow(e,-(part.getVel()[i])))
         else:
             return 2/(1+pow(e,-(part.getVel()[i]))) - 1
+
+if __name__ == "__main__":
+    tv = TextVectorizer('corpus.txt','test_text.txt','stopwords.txt','corpus_output.txt','text_output.txt',max_df=0.15,min_df=0.0002)
+    tv.init_corpus_and_text()
+    tv.fitting()
+    tv.transforming()
+    wordlist = tv.getWordList()
+    weightlist = tv.getWeightList()
+    dict_ = tv.getDict()
+    tv.writeInFile("file1.txt")
+    tv.writeTfidfInFileSorted("file2.txt")
+    textvector,feature_name,feature_tfidf = tv.getTextVector()
+
+    dim = len(feature_name)
+    #print(dim)
+    size = dim * 5
+    w = 1.4
+    c1=c2=1.2
+    gama = 0.9
+    theta = 5
+    max_vel = 1
+    iter_num = 100
+    gsbpso = GSBPSO(dim,size,iter_num,max_vel,theta,gama,c1=c1,c2=c2,w=w)
+
+    fitness_value_list,best_position = gsbpso.update()
+    print(best_position)
+    print(fitness_value_list[-1])
+   
+    
+    
--- TFIDF.py
+++ TFIDF.py
@@ -4,45 +4,25 @@ from sklearn import feature_extraction
 import numpy as np
 from pprint import  pprint
 
-
+'''
 #np.set_printoptions(threshold=np.inf)
 
 word_raw = dwd('stopwords.txt', 'verse.txt', 'out.txt')  # 测试文本
 word_raw.departing()
 words = word_raw.get_words()
 
-'''
+
 string = " ".join(words)
 words_text = []
 words_text.append(string)
 
 
-'''
 corpus_raw = dwd('stopwords.txt', 'corpus.txt', 'corpus_out.txt')  # 自制小语料库测试
 corpus_raw.departing()
 corpus = corpus_raw.get_words()
-'''
-string = " ".join(corpus)
-corpus_text = []
-corpus_text.append(string)
 
-
-corpus_raw1 = dwd('stopwords.txt', 'verse1.txt', 'verse1_out.txt')  # 自制小语料库测试
-corpus_raw1.departing()
-corpus1 = corpus_raw1.get_words()
-
-string = " ".join(corpus1)
-corpus_text1 = []
-corpus_text1.append(string)
-#words.extend(corpus)
-#print(words)
-'''
 # 构建tfidf模型
-'''
-tfidf_vec_train = TfidfVectorizer(token_pattern=r"(?u)\b\w\w+\b")
-tfidf_vec_train_vacabulary = tfidf_vec_train.fit_transform(corpus)
-voc_list = tfidf_vec_train.vocabulary_
-'''
+
 #print(voc_list)
 tfidf_vec = TfidfVectorizer(token_pattern=r"(?u)\b\w\w+\b",max_df=0.15,min_df=0.002)
 #tfidf_model = tfidf_vec.fit(corpus)
@@ -54,23 +34,23 @@ tfidf_vec.fit(corpus)
 tfidf_matrix = tfidf_vec.transform(words)
 
 #print(tfidf_vec.inverse_transform(tfidf_matrix))
-'''
+
 for i in tfidf_matrix.toarray():
     print(i)
 print(tfidf_matrix.toarray())
-'''
+
 # print(tfidf_vec.get_feature_names())
 # 词表
 wordlist = tfidf_vec.get_feature_names()
 #print(tfidf_vec.vocabulary_)
 #print(tfidf_matrix.toarray())
-
+print(type(wordlist))
 #print(wordlist)
 #print(len(wordlist))
 # 权值表
 weightlist = tfidf_matrix.toarray()
 #print(len(weightlist))
-
+print(type(weightlist))
 # tf-idf矩阵中 元素a[i][j]表示j词在i类文本中的tfidf权重
 # 写入文本
 with open("tfidf_word.txt", 'w', encoding='utf-8')as f:
@@ -99,8 +79,7 @@ for i in range(len(weightlist)):
         #dict_final[wordlist[j]] = weightlist[i][j]
 
 dict_final_sort = sorted(dict_final.items(), key=lambda x: x[1], reverse=True)
-for i in weightlist:
-    print(i)
+
 #print(dict_final_sort)
 #print(dict(dict_final_sort))
 with open("main_txt.txt",'w')as f:
@@ -108,4 +87,120 @@ with open("main_txt.txt",'w')as f:
         every=str(i)
         # print(every)
         f.write(every+'\n')
+'''
+class TextVectorizer:
+    def __init__(self,corpus_name,text_name,stopwords,corpus_output,text_output,max_df,min_df):
+        self.tfidf_vec = TfidfVectorizer(token_pattern=r"(?u)\b\w\w+\b",max_df=max_df,min_df=min_df)
+        self.corpus_name = corpus_name
+        self.text_name = text_name
+        self.stopwords = stopwords
+        self.corpus_output = corpus_output
+        self.text_output = text_output
 
+    def init_corpus_and_text(self):
+        corpus_raw = dwd(self.stopwords, self.corpus_name, self.corpus_output)  # 自制小语料库测试
+        corpus_raw.departing()
+        corpus = corpus_raw.get_words()
+        self.corpus = corpus
+
+        word_raw = dwd(self.stopwords, self.text_name, self.text_output)  # 测试文本
+        word_raw.departing()
+        words = word_raw.get_words()
+        self.text = words
+
+    def fitting(self):
+        self.tfidf_vec.fit(self.corpus)
+
+    def transforming(self):
+        self.tfidf_matrix = self.tfidf_vec.transform(self.text)
+    
+    def getWordList(self):
+        return self.tfidf_vec.get_feature_names()
+
+    def getWeightList(self):
+        return self.tfidf_matrix.toarray()
+
+    def writeInFile(self,filename):
+        weightlist = self.getWeightList()
+        wordlist = self.getWordList()
+        with open(filename, 'w', encoding='utf-8')as f:
+            for i in range(len(weightlist)):
+                f.write("第"+str(i)+"段文本："+"\n")
+                # print("------第",i,'段文本词语的tfidf权重')
+                for j in range(len(wordlist)):
+                    #print(wordlist[j],weightlist[i][j])
+                    f.write(wordlist[j]+"  "+str(weightlist[i][j])+"\n")
+
+    def getDict(self):
+        dict_final = {}
+        weightlist = self.getWeightList()
+        wordlist = self.getWordList()
+        for i in range(len(weightlist)):
+            for j in range(len(wordlist)):
+                
+                try:
+                    dict_final[wordlist[j]]+=weightlist[i][j]
+                except:
+                    dict_final[wordlist[j]]=0
+                    dict_final[wordlist[j]] += weightlist[i][j]
+        dict_final_sort = sorted(dict_final.items(), key=lambda x: x[1], reverse=True)
+        return dict_final_sort
+
+    def writeTfidfInFileSorted(self,filename):
+        dict_ = self.getDict()
+        with open(filename,'w')as f:
+            for i in list(dict_):
+                every=str(i)
+                # print(every)
+                f.write(every+'\n')
+
+    def getTextVector(self):
+        feature_name = []
+        feature_tfidf = []
+        textvector = []
+        dict_ = self.getDict()
+        
+        for i in dict_:
+            #tuple_ = ()
+            if i[1] > 0:
+                feature_name.append(i[0])
+                feature_tfidf.append(i[1])
+                
+                #tuple_ = (i)
+                
+                textvector.append(i)
+        return textvector,feature_name,feature_tfidf
+
+
+'''
+corpus_raw = dwd('stopwords.txt', 'corpus.txt', 'corpus_out.txt')  # 自制小语料库测试
+corpus_raw.departing()
+corpus = corpus_raw.get_words()
+
+word_raw = dwd('stopwords.txt', 'verse.txt', 'out.txt')  # 测试文本
+word_raw.departing()
+words = word_raw.get_words()
+'''
+if __name__ == "__main__":
+    '''
+    tv = TextVectorizer('corpus.txt','verse.txt','stopwords.txt','corpus_output.txt','text_output.txt',max_df=0.15,min_df=0.0002)
+    tv.init_corpus_and_text()
+    tv.fitting()
+    tv.transforming()
+    wordlist = tv.getWordList()
+    weightlist = tv.getWeightList()
+    dict_ = tv.getDict()
+    tv.writeInFile("file1.txt")
+    tv.writeTfidfInFileSorted("file2.txt")
+    textvector,feature_name,feature_tfidf = tv.getTextVector()
+    print(textvector)
+    print(feature_name)
+    print(feature_tfidf)
+    #print(wordlist)
+    #for i in weightlist:
+        #print(i)
+    #print(dict_)
+    #print(feature_name)
+    #print(len(feature_name))
+    '''
+    pass
